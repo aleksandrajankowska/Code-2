2af60bb9e9c851dd6698534094e86d459f64744d
janka303 <janka303@users.noreply.github.com>
2018-05-01 13:48:28 -0400

    data vis final

--- Homework/week13_DataVis/Class Data Visualization/OJTile.js
+++ Homework/week13_DataVis/Class Data Visualization/OJTile.js
@@ -25,16 +25,17 @@ class OJTile extends Tile {
 
     // compare each answer to the string 'Yes'
     // increment counters as appropriate
+    //console.log(this.hypeAnswers[1].charAt(0));
     for (let i = 0; i < this.hypeAnswers.length; i++) {
-      if (this.hypeAnswers[i] === '1 - What is hypebeast?') {
+      if (this.hypeAnswers[i].charAt(0) === '1') {
         this.answer1++;
       } else if(this.hypeAnswers[i] === '2 - Heard about it') {
       	this.answer2++;
-      } else if(this.hypeAnswers[i] === '3 – I know some things'){
+      } else if(this.hypeAnswers[i].charAt(0) === '3'){
       	this.answer3++;
-      } else if(this.hypeAnswers[i] === '4 – Got a few Supreme shirts'){
+      } else if(this.hypeAnswers[i].charAt(0) === '4'){
       	this.answer4++;
-      } else if(this.hypeAnswers[i] === '5 – Calculating potential income you can get from the next drop'){
+      } else if(this.hypeAnswers[i].charAt(0) === '5'){
       	this.answer5++;
       }
     }
@@ -46,8 +47,40 @@ class OJTile extends Tile {
   }
 
   display() {
+
+	super.display();
+
+	// img = loadImage("assets/bape.png"); 
+	// img1 = loadImage("assets/stussy.jpg"); 
+	//img2 = loadImage("assets/supreme.png"); 
+	// img3 = loadImage("assets/yeezy.png"); 
+
+  	let size = this.answer1*15;
+  	let size1 = this.answer2*15;
+  	let size2 = this.answer3*15;
+  	let size3 = this.answer4*15;
+  	let size4 = this.answer5*15;
+
+  	fill(255,0,0);
+  	textSize(18);
+  	textFont('Helvetica');
+    text('How much of a hypebeast are you?',20, 50);
+
+    noStroke();
+
+    fill(244, 215, 66);
+    rect(20, 160, size+50, size+120);
+    fill(244, 184, 65);
+    rect(70, 160, size1+50, size1+120);
+    fill(244, 139, 65);
+    rect(150, 160, size2+50, size2+120);
+    fill(244, 94, 65);
+    rect(260, 160, size3+50, size3+120);
+    fill(255, 65, 65);
+    rect(320, 160, size4+50, size4+120);
+
     // call super class' display method so we get our
     // clean matrix and translation
-    super.display();
+    
   }
 }
--- Homework/week13_DataVis/Class Data Visualization/sketch.js
+++ Homework/week13_DataVis/Class Data Visualization/sketch.js
@@ -2,6 +2,7 @@
 let tiles = [];
 let tileSize = 400;
 
+
 function preload() {
   // let table = loadTable('https://dl.dropboxusercontent.com/s/cgrl2ekchqrf9hp/Untitled%20form.csv?dl=1&raw=1', 'csv', 'header');
   let table = loadTable('https://dl.dropboxusercontent.com/s/mrq421mkmg31wng/survey.csv?dl=1&raw=1', 'csv', 'header');
@@ -9,11 +10,7 @@ function preload() {
 }
 
 function setup() {
-  tiles.push(new JBTile(0, 0, tileSize));
-  img = loadImage("assets/stussy.jpg"); 
-  img1 = loadImage("assets/bape.png"); 
-  img2 = loadImage("assets/yeezy.png"); 
-  img3 = loadImage("assets/supreme.png"); 
+  tiles.push(new OJTile(0, 0, tileSize));
 
   createCanvas(tileSize * 1, tileSize * 1);
 
@@ -21,25 +18,7 @@ function setup() {
 }
 
 function draw() {
-  background(255);
-
-  textSize(16);
-  fill(255,0,0);
-  text('How much of a Hypebeast are you?', 20, 30);
-
-  fill(0);
-
-  ellipse(50, 50, answer1, answer1);
-
-  ellipse(70, 50, answer2, answer2);
-
-  ellipse(90, 50, answer3, answer3);
-
-  ellipse(110, 50, answer4, answer4);
-
-  ellipse(130, 50, answer5, answer5);
-
-  image(img, 0, height/2, img.width/2, img.height/2);
+  background(255, 221, 188);
 
   for (var i = 0; i < tiles.length; i++) {
     tiles[i].update();
